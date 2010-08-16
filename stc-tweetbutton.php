ee473839367d7a10ac6caf880668a82b549e384c
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-16 15:15:37 +0000

    Add query parameters to twitter share link

--- stc-tweetbutton.php
+++ stc-tweetbutton.php
@@ -68,9 +68,16 @@ function get_stc_tweetbutton($args='') {
 	$post = get_post($id);
 	$text = esc_attr(strip_tags($post->post_title));;
 	
-	if (!empty($related)) $related = " data-related='{$related}'";
+	if (!empty($related)) $datarelated = " data-related='{$related}'";
 	
-	$out = "<a href='http://twitter.com/share' class='twitter-share-button' data-text='{$text}' data-url='{$url}' data-counturl='{$counturl}' data-count='{$style}' data-via='{$source}'{$related}>Tweet</a>";
+	$query = http_build_query(array(
+		'url'     => $url,
+		'text'    => $text,
+		'count'   => $style,
+		'related' => $related,
+		));
+		
+	$out = "<a href='http://twitter.com/share?{$query}' class='twitter-share-button' data-text='{$text}' data-url='{$url}' data-counturl='{$counturl}' data-count='{$style}' data-via='{$source}'{$datarelated}>Tweet</a>";
 	return $out;
 }
 
